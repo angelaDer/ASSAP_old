c6524fbb9fe0926c9d073531c9a1043669111026
angelaDer <angela.derewienczuk@smcebi.edu.pl>
2019-08-05 22:17:30 +0200

    Frontend first version

---
+++ app/src/main/java/com/example/angela/assap/LogIn.java
@@ -0,0 +1,13 @@
+package com.example.angela.assap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LogIn extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.createnew);
+    }
+}
